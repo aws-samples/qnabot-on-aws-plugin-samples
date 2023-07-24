0fa67ff70076ca9862bbaaec23e970b8e3472ccf
Bob Strahan <strahanr@amazon.com>
2023-07-24 14:44:55 +0000

    Rename llm.py files for consistency across models. Recreate llm connection when model parameters are changed

--- lambdas/ai21-llm/src/llm.py
+++ lambdas/ai21-llm/src/llm.py
@@ -2,11 +2,11 @@ import boto3
 import os
 import json
 from botocore.exceptions import ClientError
-from langchain.chat_models import ChatAnthropic
-from langchain.schema import HumanMessage
+from langchain.llms import AI21
 
 # global variables - avoid creating a new model for every request
 llm = None
+llm_params = None
 
 def get_secret(secret_name):
     secrets_client = boto3.client('secretsmanager')
@@ -23,24 +23,21 @@ def get_llm(params):
     print("Getting API key from Secrets Manager")
     secret_name = os.environ['API_KEY_SECRET_NAME']
     api_key = get_secret(secret_name)
-    os.environ['ANTHROPIC_API_KEY'] = api_key
+    os.environ['AI21_API_KEY'] = api_key
     print("Getting LLM model from LangChain")
-    model = ChatAnthropic(**params)
+    model = AI21(**params)
     return model
 
 def lambda_handler(event, context):
     print("Event: ", json.dumps(event))
     global llm
+    global llm_params
     prompt = event["prompt"]
     model_params = event["parameters"] 
-    if (llm is None):
+    if (llm is None or llm_params != model_params):
+        llm_params = model_params
         llm = get_llm(model_params)
-    messages = [
-        HumanMessage(
-            content=prompt
-        )
-    ]
-    generated_text = llm(messages).content
+    generated_text = llm(prompt)
     print("Result:", json.dumps(generated_text))
     return {
         'generated_text': generated_text
--- lambdas/anthropic-llm/src/llm.py
+++ lambdas/anthropic-llm/src/llm.py
@@ -2,10 +2,12 @@ import boto3
 import os
 import json
 from botocore.exceptions import ClientError
-from langchain.llms import AI21
+from langchain.chat_models import ChatAnthropic
+from langchain.schema import HumanMessage
 
 # global variables - avoid creating a new model for every request
 llm = None
+llm_params = None
 
 def get_secret(secret_name):
     secrets_client = boto3.client('secretsmanager')
@@ -22,19 +24,26 @@ def get_llm(params):
     print("Getting API key from Secrets Manager")
     secret_name = os.environ['API_KEY_SECRET_NAME']
     api_key = get_secret(secret_name)
-    os.environ['AI21_API_KEY'] = api_key
+    os.environ['ANTHROPIC_API_KEY'] = api_key
     print("Getting LLM model from LangChain")
-    model = AI21(**params)
+    model = ChatAnthropic(**params)
     return model
 
 def lambda_handler(event, context):
     print("Event: ", json.dumps(event))
     global llm
+    global llm_params
     prompt = event["prompt"]
     model_params = event["parameters"] 
-    if (llm is None):
+    if (llm is None or llm_params != model_params):
+        llm_params = model_params
         llm = get_llm(model_params)
-    generated_text = llm(prompt)
+    messages = [
+        HumanMessage(
+            content=prompt
+        )
+    ]
+    generated_text = llm(messages).content
     print("Result:", json.dumps(generated_text))
     return {
         'generated_text': generated_text
