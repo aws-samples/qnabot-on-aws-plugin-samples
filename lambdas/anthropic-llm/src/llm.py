5f7d7a77d48374f1ecb42c898c106fd8215bcf46
Bob Strahan <strahanr@amazon.com>
2023-07-24 17:58:34 +0000

    Refactor Anthropic LLM function to avoid LangChain or Anthropic SDK dependencies - reduce cold start latency from >20sec to ~7 sec). Remove Anthropic layer

--- lambdas/anthropic-llm/src/llm.py
+++ lambdas/anthropic-llm/src/llm.py
@@ -1,15 +1,17 @@
-import boto3
 import os
 import json
+import urllib3
+import boto3
 from botocore.exceptions import ClientError
-from langchain.chat_models import ChatAnthropic
-from langchain.schema import HumanMessage
 
-# global variables - avoid creating a new model for every request
-llm = None
-llm_params = None
+# Defaults
+API_KEY_SECRET_NAME = os.environ['API_KEY_SECRET_NAME']
+ENDPOINT_URL = os.environ.get("ENDPOINT_URL", "https://api.anthropic.com/v1/complete")
+DEFAULT_MODEL = os.environ.get("DEFAULT_MODEL","claude-instant-1")
+MAX_TOKENS_TO_SAMPLE = 256
 
 def get_secret(secret_name):
+    print("Getting API key from Secrets Manager")
     secrets_client = boto3.client('secretsmanager')
     try:
         response = secrets_client.get_secret_value(
@@ -20,30 +22,54 @@ def get_secret(secret_name):
     api_key = response['SecretString']
     return api_key
 
-def get_llm(params):
-    print("Getting API key from Secrets Manager")
-    secret_name = os.environ['API_KEY_SECRET_NAME']
-    api_key = get_secret(secret_name)
-    os.environ['ANTHROPIC_API_KEY'] = api_key
-    print("Getting LLM model from LangChain")
-    model = ChatAnthropic(**params)
-    return model
+def call_llm(parameters, prompt):
+    api_key = get_secret(API_KEY_SECRET_NAME)
+    # default parameters.. see https://docs.anthropic.com/claude/reference/complete_post for additional
+    # allowed parameters
+    data = {
+        "max_tokens_to_sample": MAX_TOKENS_TO_SAMPLE,
+        "model": DEFAULT_MODEL
+    }
+    data.update(parameters)
+    data["prompt"] = prompt
+    headers = {
+        "anthropic-version": "2023-06-01", 
+        "x-api-key": api_key,
+        "content-type": "application/json",
+        "accept": "application/json"
+    }
+    http = urllib3.PoolManager()
+    try:
+        response = http.request(
+            "POST",
+            ENDPOINT_URL,
+            body=json.dumps(data),
+            headers=headers
+        )
+        if response.status != 200:
+            raise Exception(f"Error: {response.status} - {response.data}")
+        generated_text = json.loads(response.data)["completion"].strip()
+        return generated_text
+    except Exception as err:
+        print(err)
+        raise
 
+"""
+Example Test Event:
+{
+  "prompt": "\n\nHuman:Why is the sky blue?\n\nAssistant:",
+  "parameters": {
+    "temperature": 0,
+    "model": "claude-instant-1"
+  }
+}
+"""
 def lambda_handler(event, context):
     print("Event: ", json.dumps(event))
-    global llm
-    global llm_params
+    global secret
     prompt = event["prompt"]
-    model_params = event["parameters"] 
-    if (llm is None or llm_params != model_params):
-        llm_params = model_params
-        llm = get_llm(model_params)
-    messages = [
-        HumanMessage(
-            content=prompt
-        )
-    ]
-    generated_text = llm(messages).content
+    parameters = event["parameters"] 
+    generated_text = call_llm(parameters, prompt)
     print("Result:", json.dumps(generated_text))
     return {
         'generated_text': generated_text
