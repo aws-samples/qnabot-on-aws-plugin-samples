231e83ba9b4ca1329b6dcac6141b422778878e11
Windrich <yuwindr@amazon.com>
2023-07-26 21:37:18 +0800

    updated AI21 qa prompt template and fixed typos

--- lambdas/ai21-llm/src/settings.py
+++ lambdas/ai21-llm/src/settings.py
@@ -2,8 +2,8 @@ import cfnresponse
 import json
 
 # Default prompt temnplates
-AI21_GENERATE_QUERY_PROMPT_TEMPATE = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.<br>Chat History: <br>{history}<br>Follow up question: {input}<br>Standalone question:"""
-AI21_QA_PROMPT_TEMPLATE = """<br><br>Human: You are a friendly AI assistant. You provide answers only based on the provided reference passages. Here are reference passages in <references> tags:<br><references><br>{context}<br></references><br>If the references contain the information needed to respond, then write a confident response in under 50 words, quoting the relevant references. <br>Otherwise, if you can make an informed guess based on the reference passages, then write a less condident response in under 50 words, stating your assumptions.<br>Finally, if the references do not have any relevant information, then respond saying \\"Sorry, I don't know\\".<br><br>{query}<br><br>Assistant: According to the reference passages, in under 50 words:"""
+AI21_GENERATE_QUERY_PROMPT_TEMPLATE = """Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question.<br>Chat History: <br>{history}<br>Follow up question: {input}<br>Standalone question:"""
+AI21_QA_PROMPT_TEMPLATE = """The following is a friendly conversation between a human and an AI. The AI is talkative and provides lots of specific details from its context. If the AI does not know the answer to a question, it truthfully says it does not know. Documents: {context} Instruction: Based on the above documents, provide a detailed answer for {query} Answer "don't know" if not present in the document. Solution:"""
 
 def getModelSettings(modelType):
     params = {
@@ -16,7 +16,7 @@ def getModelSettings(modelType):
     settings = {
         'LLM_GENERATE_QUERY_MODEL_PARAMS': json.dumps(params),
         'LLM_QA_MODEL_PARAMS': json.dumps(params),
-        'LLM_GENERATE_QUERY_PROMPT_TEMPLATE': AI21_GENERATE_QUERY_PROMPT_TEMPATE,
+        'LLM_GENERATE_QUERY_PROMPT_TEMPLATE': AI21_GENERATE_QUERY_PROMPT_TEMPLATE,
         'LLM_QA_PROMPT_TEMPLATE': AI21_QA_PROMPT_TEMPLATE
     }
     
